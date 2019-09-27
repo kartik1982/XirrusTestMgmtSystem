c0de6f7090fc4ddd37c39cc819216760e6e25238
Kartik Aiyar <kartik.aiyar@riverbed.com>
2019-09-27 14:43:41 -0700

    updated test case for regression failures

--- spec/XirrusMgmtSys_GUI/TS_SupportTools/TC_Customers_Scope_To_Customer.rb
+++ spec/XirrusMgmtSys_GUI/TS_SupportTools/TC_Customers_Scope_To_Customer.rb
@@ -1,11 +1,10 @@
 require_relative "../TS_Mynetwork/local_lib/ap_lib.rb"
-require_relative "../environment_variables_library.rb"
 require_relative "./local_lib/support_tools_lib.rb"
 ################################################################################################################
 ##############TEST CASE: Test the SUPPORT TOOLS area - CUSTOMERS tab - Browsing Tenant view####################
 ################################################################################################################
 describe "********** TEST CASE: Test the SUPPORT TOOLS area - CUSTOMERS tab - Browsing Tenant view **********" do
-	tenant_name = return_proper_value_based_on_the_used_environment($the_environment_used, "supportTools/customrs/", "")
+	$the_environment_used=="test01" ? tenant_name = "Macadamian Avaya Parent" : tenant_name = "Macadamian - Avaya MSP"
 	include_examples "go to support tools"
 	include_examples "search for customer", tenant_name, "1", true
 	include_examples "browse to tenant", tenant_name, "AVAYA"
