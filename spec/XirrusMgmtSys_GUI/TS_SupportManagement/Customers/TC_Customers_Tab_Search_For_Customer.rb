7b7320bdf0c8e3afb7de863214cfdf825208ec4e
Kartik Aiyar <kartik.aiyar@riverbed.com>
2019-09-27 13:33:58 -0700

    updated file

--- spec/XirrusMgmtSys_GUI/TS_SupportManagement/Customers/TC_Customers_Tab_Search_For_Customer.rb
+++ spec/XirrusMgmtSys_GUI/TS_SupportManagement/Customers/TC_Customers_Tab_Search_For_Customer.rb
@@ -1,12 +1,9 @@
 require_relative "../local_lib/support_management_lib.rb"
-require_relative "../../environment_variables_library.rb"
 ##########################################################################################################
 ###########TEST CASE: Test the SUPPORT MANAGEMENT area - CUSTOMERS TAB - SEARCH FOR CUSTOMER##############
 ##########################################################################################################
 describe "********** TEST CASE: Test the SUPPORT MANAGEMENT area - CUSTOMERS TAB - SEARCH FOR CUSTOMER **********" do
-
-	number_of_tenants = return_proper_value_based_on_the_used_environment($the_environment_used, "supportManagement/customers/customers_tab_search_for_customer.rb", "")
-
+  $the_environment_used=="test01" ? number_of_tenants=90 : number_of_tenants=23
 	include_examples "go to support management"
 	include_examples "search for customer and verify grid responds", "asdr" , 0
 	include_examples "search for customer and verify grid responds", "Adrian-Automation-Firefox" , 1
