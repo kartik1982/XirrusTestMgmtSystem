97fffebef734ad84e17f6593893111fa2b8f9b96
Kartik Aiyar <kartik.aiyar@riverbed.com>
2019-09-27 11:54:48 -0700

    updated api section for testcase

--- spec/XirrusMgmtSys_GUI/TS_Steel_Connect/General/TC_SteelConnect_First_Login_Information_Modal.rb
+++ spec/XirrusMgmtSys_GUI/TS_Steel_Connect/General/TC_SteelConnect_First_Login_Information_Modal.rb
@@ -4,7 +4,7 @@ require_relative "../local_lib/steel_connect_lib.rb"
 ###############TEST CASE: STEEL CONNECT - Create a new tenant######################################
 #####################################################################################################
 tenant_name = "Adrian-Automation-SteelConnect-Child-Domain-for-testing-the-first-login-information-modal"
-
+tenant_id =nil
 describe "********** TEST CASE: STEEL CONNECT - Create a new tenant **********" do
 
   include_examples "go to commandcenter"
@@ -15,14 +15,12 @@ end
 describe "********** TEST CASE: STEEL CONNECT - Modify the tenant to be a SteelConnect one **********" do
 
  it "For the tenant '#{tenant_name}' set the 'scLinked' flag to true" do
-    @token = API.get_backoffice_token({username: @username, password: @password, host: @xms_url})
-    @ng = API::ApiClient.new(token: @token)
-    @get_tenant_by_name = @ng.tenant_by_name(tenant_name).body
-    puts "Needed tenant ID: #{@get_tenant_by_name.to_s[18..53]}"
-    @needed_tenent_id = @get_tenant_by_name.to_s[18..53]
-    @get_tenant_by_name["data"][0]['tenantLinks']=[{"id"=> "", "tenantId"=> "#{@needed_tenent_id}", "link"=>"STEELCONNECT", "url"=> ""}]
-    @update_tenant = @ng.update_tenant2(@needed_tenent_id, @get_tenant_by_name["data"][0])
-    expect(@update_tenant.code).to eq(200)
+
+    tenant_body = JSON.parse(@api.get_tenant_by_name(tenant_name).body)['data'].first
+    tenant_id = tenant_body['id']
+    tenant_body.update({'tenantLinks' => [{"id"=> "", "tenantId"=> "#{tenant_id}", "link"=>"STEELCONNECT", "url"=> ""}]})
+    response = @api.put_update_tenant_by_tenantid(tenant_id, tenant_body)
+    expect(response.code).to eq(200)
   end
 
 end
