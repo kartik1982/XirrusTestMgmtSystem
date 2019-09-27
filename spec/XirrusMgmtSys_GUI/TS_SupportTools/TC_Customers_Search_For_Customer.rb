4afce658162254b7d6a21974461fd22721b1ca19
Kartik Aiyar <kartik.aiyar@riverbed.com>
2019-09-27 14:13:00 -0700

    value need to be passed in string instead of integer

--- spec/XirrusMgmtSys_GUI/TS_SupportTools/TC_Customers_Search_For_Customer.rb
+++ spec/XirrusMgmtSys_GUI/TS_SupportTools/TC_Customers_Search_For_Customer.rb
@@ -4,7 +4,7 @@ require_relative "./local_lib/support_tools_lib.rb"
 ################################################################################################################
 describe "********** TEST CASE: Test the SUPPORT TOOLS area - CUSTOMERS tab - Search for a Customer **********" do
 	$the_environment_used=="test01" ? tenant_name = "Macadamian Avaya Parent" : tenant_name= "Macadamian - Avaya MSP" 
-  $the_environment_used=="test01" ? tenant_count=7 : tenant_count=8
+  $the_environment_used=="test01" ? tenant_count= "7" : tenant_count= "8"
 
 	include_examples "go to support tools"
 	include_examples "search for customer", tenant_name, "1", true
